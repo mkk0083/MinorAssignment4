66479eb6482785f8074f75fe2b60cc713f04920b
Charles Bido <charlesbido@me.com>
2015-02-11 09:33:46 -0600

    init commit

---
+++ main.c
@@ -0,0 +1,51 @@
+/**
+ * Write a program that takes in an advanced bash program and outputs
+ * a standard bash program that has no syntax errors
+ * (a.k.a create a Bash++ to Bash transpiler)
+ *
+ * Created by:
+ * --------------------
+ * Charles Bido
+ * Sam Epstein
+ * Mayur Kotamraju
+ * (TJ) Emanuel Taylor  
+ * --------------------
+ * due next Monday
+ *
+ * FEATURES TO IMPLEMENT
+ * ---------------------
+ * - bash does not allow spaces, tabs, or newlines around
+ *   the "=" symbol used in initialization
+ * - the "then" keyword in bash if statements must be on a different line
+ *   than the the "if" itself
+ * - Also, the brackets ([,]) in bash’s conditionals must have spaces
+ *   between the bracket and the condition itself.
+ * - 
+ * 
+ * Notes:
+ * - use redirection instead of fileio
+ * - allow newlines in redirection 
+ * - arg 1 is the bashfile++
+ * - arg 2...n is the bashfile++ arguments
+ */
+
+// (i.e of valid bash++ syntax but invalid bash syntax)
+// num    =                0            
+//     if [x -lt 5] then ...
+
+// Your preprocessor will need to change those two statements 
+// (and others like them) in the output to be:
+// 
+// num=0
+// if [ x -lt 5 ]
+//     then
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char const *argv[]) {
+   /* code */
+   return 0;
+}
+
